32e231f69913b0f3770746407423a510c46316f7
Amadeja <obsteter.amadeja@gmail.com>
2023-12-19 07:00:48 +0100

    add a navbar with links on the page

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import { Container } from 'react-bootstrap'
 import { Home } from "./pages/Home"
 import { About } from "./pages/About"
 import { Store } from "./pages/Store"
+import { Navbar } from "./components/Navbar"
 
 function App() {
-  return <Container className="mb-4">
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/store" element={<Store />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  </Container>
+  return (
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
+  )
 }
 
 export default App
