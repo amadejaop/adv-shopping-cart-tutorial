461eeb24c8104363ff7e58fc8201c4b4ea474a09
Amadeja <obsteter.amadeja@gmail.com>
2023-12-19 06:16:49 +0100

    add pages and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import { Home } from "./pages/Home"
+import { About } from "./pages/About"
+import { Store } from "./pages/Store"
+
 function App() {
-  return <h1>Hi!</h1>
+  return <Container className="mb-4">
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/store" element={<Store />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </Container>
 }
 
 export default App
